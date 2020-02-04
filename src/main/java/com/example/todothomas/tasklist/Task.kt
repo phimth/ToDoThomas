f24b7165354685172a5141ff913022ac7786c7ec
Thomas PHIMANESONE <20150195@B15-09.local>
2020-02-04 16:06:29 +0100

    commit 1

--- src/main/java/com/example/todothomas/tasklist/Task.kt
+++ src/main/java/com/example/todothomas/tasklist/Task.kt
@@ -1,2 +1,5 @@
 package com.example.todothomas.tasklist
 
+data class Task(val id: String,val title: String, val description: String = "valeur par défaut"){
+
+}
--- src/main/java/com/example/todothomas/tasklist/TaskListAdapter.kt
+++ src/main/java/com/example/todothomas/tasklist/TaskListAdapter.kt
@@ -1,4 +1,32 @@
 package com.example.todothomas.tasklist
 
-class TaskListAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todothomas.R
+import kotlinx.android.synthetic.main.item_task.view.*
+
+class TaskListAdapter(private val taskList: List<Task>) : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.bind(taskList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return taskList.size
+    }
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(itemView)
+    }
+
+    inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(taskTitle: Task) {
+            itemView.task_title.text = taskTitle.toString()
+        }
+    }
+
 }
